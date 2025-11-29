35de210ab2d74bd508fb4f676d8346d3cabaa7c6
vcuin97 <vcuin@csumb.edu>
2025-11-28 18:05:27 -0800

    Fix: add frontend files properly

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.js';
+
+function App(){
+  return(
+    <div className = "App">
+      <header className="App-header">
+        <h1>日本語 Practice</h1>
+        <p>learn N2 Vocab</p>
+        <button>Practice Sets</button>
+      </header>
+    </div>
+
+  );
+}
+export default App;
